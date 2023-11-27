902a51d5f9306db619a0cdc638b609b84c6bb1b0
Imtiaz Nayeem Shawon <89548269+ImtiazNayeemShawon@users.noreply.github.com>
2023-11-27 23:20:55 +0600

    Day7

---
+++ Day7/solution.js
@@ -0,0 +1,17 @@
+var reduce = function (nums, fn, init) {
+  if (nums.length === 0) {
+    return init;
+  }
+  let result = init;
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]);
+  }
+
+  console.log(result);
+};
+
+nums = [1, 2, 3, 4];
+fn = function sum(accum, curr) {
+  return accum + curr;
+};
+init = 0;
